d27a62b210b6c57a7d48cfe1ae2093dfd9d6dac6
Devin <devinpquinn4@gmail.com>
2024-12-14 12:26:39 -0500

    interim

---
+++ Assets/Scripts/ShootoutManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootoutManager : MonoBehaviour
+{
+    public Animator shootoutAnim;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Idle();
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Shoot_L();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Shoot_R();
+        }
+    }
+
+    public void Idle()
+    {
+        shootoutAnim.Play("Shootout_Idle");
+    }
+
+    public void Shoot_L()
+    {
+        shootoutAnim.Play("Shootout_Shoot_L");
+    }
+
+    public void Shoot_R()
+    {
+        shootoutAnim.Play("Shootout_Shoot_R");
+    }
+}
