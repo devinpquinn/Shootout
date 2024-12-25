f791e4ffeb85a2d67580694515cfc0d190899719
Devin <devinpquinn4@gmail.com>
2024-12-24 23:58:40 -0500

    bubble flash

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ColorChanger : MonoBehaviour
 {
@@ -8,18 +10,32 @@ public class ColorChanger : MonoBehaviour
     private Color backgroundColorDefault;
     public Color backgroundColorDark;
 
+    public Image bubble;
+    private Sprite bubbleDefault;
+    public Sprite bubbleDark;
+
+    public TextMeshProUGUI tmp;
+    private Color tmpColorDefault;
+    private Color tmpColorDark = Color.white;
+
     private void Awake()
     {
         backgroundColorDefault = background.color;
+        bubbleDefault = bubble.sprite;
+        tmpColorDefault = tmp.color;
     }
 
     public void MuzzleFlash_Darken()
     {
         background.color = backgroundColorDark;
+        bubble.sprite = bubbleDark;
+        tmp.color = tmpColorDark;  
     }
 
     public void MuzzleFlash_Restore()
     {
         background.color = backgroundColorDefault;
+        bubble.sprite = bubbleDefault;
+        tmp.color = tmpColorDefault;
     }
 }
